09ddbb0324b8d7c5ac905efb000af6bb1cfe39d4
Guanglin Du <guanglin.du@gmail.com>
2022-11-05 11:43:06 +0800

    Create the hello-world demo in Go

---
+++ README.md
@@ -0,0 +1,3 @@
+### Hello Gopher
+
+A Golang starter.
---
+++ hello.go
@@ -0,0 +1,12 @@
+/**
+ * Created on 20221105 Sat.
+ * Get Started with Go by Andrew Gerrand
+ *  - https://www.youtube.com/watch?v=2KmHtgtEZ1s&t=829s
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Greetings, fellow gopher. 20221105 Stat.")
+}
